efd823b8661170a20c5998711f6f2662045accd2
Michael Zhang <michael13162@gmail.com>
2017-05-02 10:00:08 -0600

    Added legacy creators

--- src/main/java/edu/utah/ece/async/sboldesigner/sbol/editor/dialog/PartEditDialog.java
+++ src/main/java/edu/utah/ece/async/sboldesigner/sbol/editor/dialog/PartEditDialog.java
@@ -684,6 +684,11 @@ public class PartEditDialog extends JDialog implements ActionListener, DocumentL
 					"SBOLDesigner is a simple, biologist-friendly CAD software tool for creating and manipulating the sequences of genetic constructs using the Synthetic Biology Open Language (SBOL) 2.0 data model. Throughout the design process, SBOL Visual symbols, a system of schematic glyphs, provide standardized visualizations of individual parts. SBOLDesigner completes a workflow for users of genetic design automation tools. It combines a simple user interface with the power of the SBOL standard and serves as a launchpad for more detailed designs involving simulations and experiments. Some new features in SBOLDesigner are SynBioHub integration, local repositories, importing of parts/sequences from existing files, import and export of GenBank and FASTA files, extended role ontology support, the ability to partially open designs with multiple root ComponentDefinitions, backward compatibility with SBOL 1.1, and versioning.");
 			designerAgent.createAnnotation(new QName("http://www.w3.org/ns/prov#", "creator", "prov"), "Michael Zhang");
 			designerAgent.createAnnotation(new QName("http://www.w3.org/ns/prov#", "creator", "prov"), "Chris Myers");
+			designerAgent.createAnnotation(new QName("http://www.w3.org/ns/prov#", "creator", "prov"), "Michal Galdzicki");
+			designerAgent.createAnnotation(new QName("http://www.w3.org/ns/prov#", "creator", "prov"), "Bryan Bartley");
+			designerAgent.createAnnotation(new QName("http://www.w3.org/ns/prov#", "creator", "prov"), "Sean Sleight");
+			designerAgent.createAnnotation(new QName("http://www.w3.org/ns/prov#", "creator", "prov"), "Evren Sirin");
+			designerAgent.createAnnotation(new QName("http://www.w3.org/ns/prov#", "creator", "prov"), "John Gennari");
 		}
 
 		// get/create the activity
