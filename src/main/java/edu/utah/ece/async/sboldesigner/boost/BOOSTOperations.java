812e7abd9b3e87aee45fc9ecc27053e1314b496e
Prem <premprakash0796@gmail.com>
2018-07-05 21:18:54 +0530

    handled exception due to current design sbol document

--- src/main/java/edu/utah/ece/async/sboldesigner/boost/BOOSTOperations.java
+++ src/main/java/edu/utah/ece/async/sboldesigner/boost/BOOSTOperations.java
@@ -1,9 +1,14 @@
 package edu.utah.ece.async.sboldesigner.boost;
 
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+
+import org.json.JSONException;
 import org.json.JSONObject;
+import org.sbolstandard.core2.SBOLConversionException;
 import org.sbolstandard.core2.SBOLDocument;
 
+import edu.utah.ece.async.sboldesigner.sbol.editor.SBOLEditorPreferences;
 import gov.doe.jgi.boost.client.BOOSTClient;
 import gov.doe.jgi.boost.client.utils.JsonResponseParser;
 import gov.doe.jgi.boost.enums.FileFormat;
@@ -15,20 +20,23 @@ import gov.doe.jgi.boost.exception.BOOSTClientException;
 public class BOOSTOperations {
 
 	static BOOSTClient client = new BOOSTClient(new BOOSTPreferences().getBOOSTToken());
+	
+	static String userNamespace = SBOLEditorPreferences.INSTANCE.getUserInfo().getURI().toString();
 
 	public static void codonJuggling(SBOLDocument currentDesign, boolean annotation, Strategy strategy, String host) {
 		String codonJuggleJobUUID = null;
-		try {
-			codonJuggleJobUUID = client.codonJuggle(
-					currentDesign,                 // input sequences
-					annotation,               // exclusively 5'-3' coding sequences
-					strategy,                 // codon selection strategy
-					host,                     // predefined host
-					FileFormat.SBOL);
-		} catch (BOOSTClientException | BOOSTBackEndException | IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+			try {
+				codonJuggleJobUUID = client.codonJuggle(
+						currentDesign,            // input sequences
+						annotation,               // exclusively 5'-3' coding sequences
+						strategy,                 // codon selection strategy
+						host,                     // predefined host
+						FileFormat.SBOL);
+			} catch (JSONException | SBOLConversionException | BOOSTClientException | 
+					       BOOSTBackEndException | IOException e) {
+				
+				e.printStackTrace();
+			}
 
 		if (codonJuggleJobUUID != null) {
 			checkJobReport(codonJuggleJobUUID);
@@ -37,15 +45,17 @@ public class BOOSTOperations {
 
 	public static void dnaVerification(SBOLDocument currentDesign, Vendor vendor, String sequencePatterns) {
 		String dnaVarificationJobUUID = null;
-		try {
-			dnaVarificationJobUUID = client.dnaVarification(
-					currentDesign,                  // input sequence
-					vendor,                    // vendor
-					sequencePatterns);         // sequence patterns
-		} catch (BOOSTClientException | BOOSTBackEndException | IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+			try {
+				dnaVarificationJobUUID = client.dnaVarification(
+						currentDesign,             // input sequence
+						vendor,                    // vendor
+						sequencePatterns);
+			} catch (JSONException | SBOLConversionException | BOOSTClientException | 
+					BOOSTBackEndException | IOException e) {
+				
+				e.printStackTrace();
+			}  
+			
 		if (dnaVarificationJobUUID != null) {
 			checkJobReport(dnaVarificationJobUUID);
 		}
@@ -53,44 +63,25 @@ public class BOOSTOperations {
 
 	public static void polishing(SBOLDocument currentDesign, boolean annotation, Vendor vendor, Strategy strategy, String host) {
 		String polishDNAJobUUID = null;
-		try {
-			polishDNAJobUUID = client.polish(
-					currentDesign,                  // input sequence
-					annotation,                // encoding sequences support sequence feature annotations
-					vendor,                    // vendor
-					strategy,                  // codon selection strategy
-					FileFormat.SBOL,           // output format
-					host);                     // predefined host
-		} catch (BOOSTClientException | BOOSTBackEndException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+
+			try {
+				polishDNAJobUUID = client.polish(
+						currentDesign,             // input sequence
+						annotation,                // encoding sequences support sequence feature annotations
+						vendor,                    // vendor
+						strategy,                  // codon selection strategy
+						FileFormat.SBOL,           // output format
+						host);
+			} catch (JSONException | UnsupportedEncodingException | SBOLConversionException | 
+					BOOSTClientException | BOOSTBackEndException e) {
+		
+				e.printStackTrace();
+			}       
+			
 		if (polishDNAJobUUID != null) {
 			checkJobReport(polishDNAJobUUID);
 		}
 	}
-
-
-	// // partitioning of DNA
-	// String partitiongDNAJobUUID = client.partition(
-	// selectedFilePath, // input sequence
-	// "aaacccgggttt", // 5-prime-vector-overlap
-	// "tttgggcccaaa", // 3-prime-vector-overlap
-	// Integer.toString(15), // min-BB-length
-	// Integer.toString(3000), // max-BB-length
-	// Double.toString(4.0), // minimum overlap GC
-	// Double.toString(40.0), // optimum overlap GC
-	// Double.toString(62.0), // maximum overlap GC
-	// Integer.toString(5), // minimum overlap length
-	// Integer.toString(25), // optimum overlap length
-	// Integer.toString(30), // maximum overlap length
-	// Integer.toString(20), // min. primer length
-	// Integer.toString(40), // max. primer length
-	// Integer.toString(60)); // max. primer Tm
-	// if (null != partitiongDNAJobUUID) {
-	// jobUUIDs.add(partitiongDNAJobUUID);
-	// System.out.println("Data for Partation :" + partitiongDNAJobUUID);
-	// }
 
 	static void checkJobReport(String jobUUID) {
 		JSONObject jobReport = null;
