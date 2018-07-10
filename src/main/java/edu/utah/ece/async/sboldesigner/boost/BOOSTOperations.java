0fabf722de80a2f1a5b035c20fc9a0517a060f9f
Prem <premprakash0796@gmail.com>
2018-07-10 09:41:39 +0530

    fetching URIs of components in document returned by BOOST

--- src/main/java/edu/utah/ece/async/sboldesigner/boost/BOOSTOperations.java
+++ src/main/java/edu/utah/ece/async/sboldesigner/boost/BOOSTOperations.java
@@ -1,13 +1,23 @@
 package edu.utah.ece.async.sboldesigner.boost;
 
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.UnsupportedEncodingException;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.util.Set;
 
+import org.apache.commons.httpclient.URI;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.sbolstandard.core2.ComponentDefinition;
 import org.sbolstandard.core2.SBOLConversionException;
 import org.sbolstandard.core2.SBOLDocument;
+import org.sbolstandard.core2.SBOLReader;
+import org.sbolstandard.core2.SBOLValidationException;
 
+import edu.utah.ece.async.sboldesigner.sbol.editor.SBOLDesign;
 import edu.utah.ece.async.sboldesigner.sbol.editor.SBOLEditorPreferences;
 import gov.doe.jgi.boost.client.BOOSTClient;
 import gov.doe.jgi.boost.client.utils.JsonResponseParser;
@@ -22,6 +32,7 @@ public class BOOSTOperations {
 	static BOOSTClient client = new BOOSTClient(new BOOSTPreferences().getBOOSTToken());
 	
 	static String userNamespace = SBOLEditorPreferences.INSTANCE.getUserInfo().getURI().toString();
+	
 
 	public static void codonJuggling(SBOLDocument currentDesign, boolean annotation, Strategy strategy, String host) {
 		String codonJuggleJobUUID = null;
@@ -97,11 +108,32 @@ public class BOOSTOperations {
 				}
 			}
 		} catch (BOOSTClientException | BOOSTBackEndException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		 
 		// output of the job report (which is a JSON object)
-		String resopnse = JsonResponseParser.parseCodonJuggleResponse(jobReport);
-		System.out.println(resopnse);
-	}
+		String response = JsonResponseParser.parseCodonJuggleResponse(jobReport);
+		// convert String into InputStream
+		InputStream stream = new ByteArrayInputStream(response.getBytes(StandardCharsets.UTF_8));
+		// extracting the returned SBOL string into an SBOLDocument
+		try {
+			SBOLDocument modifiedDocument = SBOLReader.read(stream);
+			String boostUri = "https://boost.jgi.doe.gov/";
+			java.net.URI rootUri = null;
+			
+			Set<ComponentDefinition> componentDef = modifiedDocument.getRootComponentDefinitions();
+			for(ComponentDefinition comDefination :componentDef) {
+				if(comDefination.getIdentity().toString() == boostUri) {
+					rootUri = comDefination.getIdentity();	
+				}
+			}
+			if(rootUri.toString() != null && !rootUri.toString().isEmpty()) {
+				// call the load method
+			}
+			
+		} catch (SBOLValidationException | IOException | SBOLConversionException e) {
+			e.printStackTrace();
+		}
+		System.out.println(response);
+	}	
 }
