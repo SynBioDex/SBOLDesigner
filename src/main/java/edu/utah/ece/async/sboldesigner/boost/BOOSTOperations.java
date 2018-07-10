a9397d41316b011bf8bbbaaa68a87a957ccc49e0
Prem <premprakash0796@gmail.com>
2018-07-10 14:07:36 +0530

    sending targetNamespace argument in BOOST methods

--- src/main/java/edu/utah/ece/async/sboldesigner/boost/BOOSTOperations.java
+++ src/main/java/edu/utah/ece/async/sboldesigner/boost/BOOSTOperations.java
@@ -4,11 +4,10 @@ import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.UnsupportedEncodingException;
-import java.net.URL;
+import java.net.URI;
 import java.nio.charset.StandardCharsets;
 import java.util.Set;
 
-import org.apache.commons.httpclient.URI;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.sbolstandard.core2.ComponentDefinition;
@@ -32,6 +31,7 @@ public class BOOSTOperations {
 	static BOOSTClient client = new BOOSTClient(new BOOSTPreferences().getBOOSTToken());
 	
 	static String userNamespace = SBOLEditorPreferences.INSTANCE.getUserInfo().getURI().toString();
+	private static String targetNamespace = "https://boost.jgi.doe.gov/";
 	
 
 	public static void codonJuggling(SBOLDocument currentDesign, boolean annotation, Strategy strategy, String host) {
@@ -39,6 +39,7 @@ public class BOOSTOperations {
 			try {
 				codonJuggleJobUUID = client.codonJuggle(
 						currentDesign,            // input sequences
+						targetNamespace,
 						annotation,               // exclusively 5'-3' coding sequences
 						strategy,                 // codon selection strategy
 						host,                     // predefined host
@@ -59,6 +60,7 @@ public class BOOSTOperations {
 			try {
 				dnaVarificationJobUUID = client.dnaVarification(
 						currentDesign,             // input sequence
+						targetNamespace,
 						vendor,                    // vendor
 						sequencePatterns);
 			} catch (JSONException | SBOLConversionException | BOOSTClientException | 
@@ -78,6 +80,7 @@ public class BOOSTOperations {
 			try {
 				polishDNAJobUUID = client.polish(
 						currentDesign,             // input sequence
+						targetNamespace,
 						annotation,                // encoding sequences support sequence feature annotations
 						vendor,                    // vendor
 						strategy,                  // codon selection strategy
@@ -115,22 +118,18 @@ public class BOOSTOperations {
 		String response = JsonResponseParser.parseCodonJuggleResponse(jobReport);
 		// convert String into InputStream
 		InputStream stream = new ByteArrayInputStream(response.getBytes(StandardCharsets.UTF_8));
-		// extracting the returned SBOL string into an SBOLDocument
+
 		try {
+			// extracting the returned SBOL string into an SBOLDocument
 			SBOLDocument modifiedDocument = SBOLReader.read(stream);
 			String boostUri = "https://boost.jgi.doe.gov/";
-			java.net.URI rootUri = null;
 			
 			Set<ComponentDefinition> componentDef = modifiedDocument.getRootComponentDefinitions();
-			for(ComponentDefinition comDefination :componentDef) {
-				if(comDefination.getIdentity().toString() == boostUri) {
-					rootUri = comDefination.getIdentity();	
-				}
-			}
-			if(rootUri.toString() != null && !rootUri.toString().isEmpty()) {
-				// call the load method
-			}
-			
+			for(ComponentDefinition componentDefination : componentDef) {
+				//each root CD, you should look for whether there is a wasDerivedFrom link (CD.getWasDerivedFroms()) 
+				//to another one of the roots
+					   
+			}	
 		} catch (SBOLValidationException | IOException | SBOLConversionException e) {
 			e.printStackTrace();
 		}
